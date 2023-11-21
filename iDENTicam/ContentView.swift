66b8e97198dd322d49286744fc95d4a98abe7c66
Nikhil <namnik100@gmail.com>
2023-11-21 10:26:11 -0800

    initial commit

---
+++ iDENTicam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iDENTicam
+//
+//  Created by Nikhil Sinha on 11/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iDENTicam/iDENTicamApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDENTicamApp.swift
+//  iDENTicam
+//
+//  Created by Nikhil Sinha on 11/21/23.
+//
+
+import SwiftUI
+
+@main
+struct iDENTicamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
